3fe582257fbc39737ea8f87018d2f013b6448b6d
Jack Huynh <vhuynh@unb.ca>
2024-09-19 16:47:18 -0300

    Update settings.py

--- Test/code/settings.py
+++ Test/code/settings.py
@@ -38,7 +38,7 @@ UPGRADE_BG_COLOR_SELECTED = '#EEEEEE'
 
 # weapons 
 weapon_data = {
-	'sword': {'cooldown': 100, 'damage': 15,'graphic':'../graphics/weapons/sword/full.png'},
+	'sword': {'cooldown': 999, 'damage': 15,'graphic':'../graphics/weapons/sword/full.png'},
 	'lance': {'cooldown': 400, 'damage': 30,'graphic':'../graphics/weapons/lance/full.png'},
 	'axe': {'cooldown': 300, 'damage': 20, 'graphic':'../graphics/weapons/axe/full.png'},
 	'rapier':{'cooldown': 50, 'damage': 8, 'graphic':'../graphics/weapons/rapier/full.png'},
